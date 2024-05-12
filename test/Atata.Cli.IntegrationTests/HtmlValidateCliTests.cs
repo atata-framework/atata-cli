f2abf81c4e01875fbaa20522ef8cb961d70fd9c5
YevgeniyShunevych <yevgeniy.shunevych@gmail.com>
2024-05-12 15:17:28 +0200

    Add `HtmlValidateCliTests`

---
+++ test/Atata.Cli.IntegrationTests/HtmlValidateCliTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace Atata.Cli.IntegrationTests;
+
+public class HtmlValidateCliTests
+{
+    [Test]
+    public void Execute()
+    {
+        ProgramCli cli = new("html-validate", useCommandShell: true)
+        {
+            ResultValidationRules = CliCommandResultValidationRules.NoError,
+            Encoding = Encoding.UTF8
+        };
+
+        string htmlPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "reddit-frontpage.html");
+
+        var result = cli.Execute($"\"{htmlPath}\" -f json");
+
+        int length = result.Output.Length;
+        TestContext.WriteLine($"Output length: {length}");
+        Assert.That(length, Is.GreaterThan(100_000));
+    }
+}
